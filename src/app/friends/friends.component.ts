969b2c376883985058b5d13f19d10bed522fc595
konstantin <kos5tya@yandex.ru>
2017-04-10 23:04:35 +0300

    add friends component

---
+++ src/app/friends/friends.component.ts
@@ -0,0 +1,27 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {AsyncService} from '../shared/models/async.service';
+import {IProfileItem} from '../shared/models/iparty';
+
+@Component({
+  selector: 'app-friends',
+  templateUrl: './friends.component.html',
+  styleUrls: ['./friends.component.css']
+})
+export class FriendsComponent implements OnInit {
+
+  @Input() friends: IProfileItem[] = [];
+
+  constructor(private _AsyncService: AsyncService ) { }
+
+  ngOnInit() {
+    this.getFriendsList();
+  }
+
+  private getFriendsList() {
+    this._AsyncService.getFriends()
+      .subscribe((data) => {
+        this.friends = data;
+      });
+  }
+
+}
